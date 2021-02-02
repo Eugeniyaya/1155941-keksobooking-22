8aab16fa95eeb9fe3cd7dd122927a87f4e609558
Evgeniya <54440430+Eugeniyaya@users.noreply.github.com>
2021-02-02 17:32:34 +0300

    2 функции
    1. Функция, возвращающая случайное целое число из переданного диапазона включительно.
    2. Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно с указанным количеством знаков после запятой.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+'Use strict';
+const getRandomNumber = function (min, max) {
+  if (min<=max) {
+    let temp = min;
+    min = max;
+    max = temp;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const getRandomFraction = function (min, max, quantity) {
+  if (min<=max) {
+    let temp = min;
+    min = max;
+    max = temp;
+  }
+  let num = Math.random() * (max - min) + min;
+  return +num.toFixed(quantity);
+}
