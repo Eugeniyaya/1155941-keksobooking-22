6369f0b42d6392eacd75de2a3cb41e9eb68d291a
Evgeniya <54440430+Eugeniyaya@users.noreply.github.com>
2021-03-15 12:54:04 +0300

    правки по отправке формы

--- js/form.js
+++ js/form.js
@@ -1,23 +1,27 @@
 import {sendData} from './create-fetch.js';
-import {showAlert} from './util.js';
 
 const form = document.querySelector('.ad-form');
 const sendDataButton = form.querySelector('.ad-form__submit');
 const resetFormButton = form.querySelector('.ad-form__reset');
 
-const setUserFormSubmit = (onSuccess) => {
+const onSuccessSendAd = () => {alert('Данные отправлены')};
+const onErrorSendAd = () => {alert('Упс... ошибочка')};
+
+const setUserFormSubmit = () => {
   sendDataButton.addEventListener('submit', (evt) => {
     evt.preventDefault();
 
-    sendData(
-      () => onSuccess(),
-      () => showAlert('Не удалось отправить форму. Попробуйте ещё раз'),
-      new FormData(evt.target),
-    );
+    sendData(onSuccessSendAd, onErrorSendAd, new FormData(evt.target));
     form.reset();
   });
 };
 
-export {setUserFormSubmit}
+const resetUserFormSubmit = () => {
+  resetFormButton.addEventListener('click', () => {
+    form.reset();
+  })
+}
+
+export {setUserFormSubmit, resetUserFormSubmit}
 
 
